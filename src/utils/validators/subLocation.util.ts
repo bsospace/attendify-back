a2f7eb1048f59d54b683053b1adb885eb358ad5e
Piyawat Wongyat <mask30254@gmail.com>
2024-12-15 23:18:45 +0700

    Revert "✨ feat: Implement location and sublocation routes with validation"

--- src/utils/validators/subLocation.util.ts
+++
@@ -1,5 +0,0 @@
-import { body } from "express-validator";
-
-export const subLocationValidationRules = () => [
-    body("name").notEmpty().withMessage("Name is required!").isString().withMessage("Name must be a string"),
-];
