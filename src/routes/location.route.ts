7faf60233de19a2c788e14cf3d014b878d4335e1
boytur <104257779+boytur@users.noreply.github.com>
2025-01-05 19:32:17 +0700

    ✨ feat[location.route.ts]: add authentication and permission checks for location routes

--- src/routes/location.route.ts
+++ src/routes/location.route.ts
@@ -4,18 +4,34 @@ import { locationValidationRules, locationIdValidationRules } from "../utils/val
 import { validateRequest } from "../middlewares/validate.middleware";
 import { LocationService } from "../services/location.service";
 import { LocationController } from "../controllers/location.controller";
+import { requirePermission } from "../middlewares/role.middleware";
+import { Permissions } from "../utils/permission.util";
+import AuthMiddleware from "../middlewares/auth.middleware";
+import { UserService } from "../services/user.service";
+import { CryptoService } from "../services/crypto.service";
+import { AuthService } from "../services/auth.service";
 
 const router = Router();
 const locationService = new LocationService();
 const locationController = new LocationController(locationService);
 
+const userService = new UserService();
+const cryptoService = new CryptoService();
+const authService = new AuthService();
+
+const authMiddleware = new AuthMiddleware(userService, cryptoService, authService);
+
 router.get(
     "/",
+    authMiddleware.authenticate,
+    requirePermission(Permissions.READ_LOCATIONS),
     locationController.getAllLocations,
 )
 
 router.get(
     "/:id",
+    authMiddleware.authenticate,
+    requirePermission(Permissions.READ_LOCATIONS),
     locationIdValidationRules(),
     validateRequest,
     locationController.getLocation,
@@ -23,6 +39,8 @@ router.get(
 
 router.post(
     "/create",
+    authMiddleware.authenticate,
+    requirePermission(Permissions.CREATE_LOCATIONS),
     locationValidationRules(),
     validateRequest,
     locationController.createLocation
@@ -30,6 +48,8 @@ router.post(
 
 router.put(
     "/:id/edit",
+    authMiddleware.authenticate,
+    requirePermission(Permissions.UPDATE_LOCATIONS),
     locationValidationRules(),
     validateRequest,
     locationController.editLocation
@@ -37,6 +57,8 @@ router.put(
 
 router.delete(
     "/:id/delete",
+    authMiddleware.authenticate,
+    requirePermission(Permissions.DELETE_LOCATIONS),
     locationIdValidationRules(),
     validateRequest,
     locationController.deleteLocation
