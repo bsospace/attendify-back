0c1b9b7b7fc4a67bc5f33f7b2efc3049eb060e93
Piyawat Wongyat <mask30254@gmail.com>
2025-02-06 21:45:23 +0700

    ✨ feat[group.util.ts]: add validation rules for update group endpoint and include optional logs parameter in getGroupsValidationRules

--- src/utils/validators/group.util.ts
+++ src/utils/validators/group.util.ts
@@ -1,13 +1,20 @@
-import { body, query } from "express-validator";
+import { body, param, query } from "express-validator";
 
 // Validation rules for the login endpoint
 export const getGroupsValidationRules = () => [
     query("page").optional().isNumeric().withMessage("Page must be a number"),
     query("pageSize").optional().isNumeric().withMessage("Page size must be a number"),
     query("search").optional().isString().withMessage("Search must be a string"),
+    query("logs").optional().isBoolean().withMessage("Logs must be a boolean"),
 ];
 
 // Validation rules for the create group endpoint
 export const createGroupValidationRules = () => [
     body("name").notEmpty().withMessage("Name is required!").isString().withMessage("Name must be a string"),
+];
+
+// Validation rules for the update group endpoint
+export const updateGroupValidationRules = () => [
+    body("name").optional().isString().withMessage("Name must be a string"),
+    param("id").notEmpty().withMessage("ID is required!").isString().withMessage("ID must be a string"),
 ];
