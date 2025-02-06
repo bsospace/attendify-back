620ca91de93c51b3e618389625d60ad6de55ff03
Piyawat Wongyat <mask30254@gmail.com>
2025-02-06 19:15:29 +0700

    ✨ feat[group.controller.ts]: add updateGroup method and standardize error response key to 'error'

--- src/controllers/group.controller.ts
+++ src/controllers/group.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { GroupService } from '../services/group.service';
-import { create } from 'domain';
 
 export class GroupController {
 
@@ -10,9 +9,16 @@ export class GroupController {
         // Bind class methods
         this.getAllGroups = this.getAllGroups.bind(this);
         this.createGroup = this.createGroup.bind(this);
+        this.updateGroup = this.updateGroup.bind(this);
     }
 
-    // Get all groups with pagination and search
+    /**
+     *  Get all groups with pagination and search
+     * @param req - Express Request object
+     * @param res - Express Response object
+     * @returns - JSON response with groups data
+     */
+
     public async getAllGroups(req: Request, res: Response): Promise<any> {
         const page: number = parseInt(req.query.page as string) || 1;
         const pageSize: number = parseInt(req.query.pageSize as string) || 10;
@@ -36,7 +42,13 @@ export class GroupController {
         }
     }
 
-    // Create a new group
+    /**
+     * Create a new group
+     * @param req - Express Request object
+     * @param res - Express Response object 
+     * @returns - JSON response with new group data 
+     */
+
     public async createGroup(req: Request, res: Response): Promise<any> {
         const { name } = req.body;
 
@@ -48,7 +60,7 @@ export class GroupController {
                 return res.status(400).json({
                     success: false,
                     message: "Create group failed",
-                    errors: "Group name already exists",
+                    error: "Group name already exists",
                 });
             }
 
@@ -57,7 +69,7 @@ export class GroupController {
                     action: "created",
                     created_at: new Date(),
                     updated_at: new Date(),
-                    created_by: req.user?.id || 'unknown',
+                    created_by: req.user?.email || 'unknown',
                 }
             ]
 
@@ -70,6 +82,50 @@ export class GroupController {
         } catch (error) {
             console.error("Error creating group:", error);
             return res.status(500).json({ message: "Failed to create group." });
+        }
+    }
+
+
+    /**
+     * Update a group
+     * @param req - Express Request object
+     * @param res - Express Response object
+     * @returns - JSON response with updated group data
+     */
+
+    public async updateGroup(req: Request, res: Response): Promise<any> {
+        const { id } = req.params;
+        const { name } = req.body;
+
+        try {
+            const group = await this.groupService.getById(id);
+
+            if (group === null) {
+                return res.status(404).json({
+                    success: false,
+                    message: "Update group failed",
+                    error: "Group not found",
+                });
+            }
+
+            const dataLogs = [
+                {
+                    action: "updated",
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                    created_by: req.user?.email || 'unknown',
+                }
+            ]
+
+            const updatedGroup = await this.groupService.updateGroup(id, { name }, dataLogs);
+            return res.status(200).json({
+                message: "Group updated successfully",
+                data: updatedGroup,
+            });
+
+        } catch (error) {
+            console.error("Error updating group:", error);
+            return res.status(500).json({ message: "Failed to update group." });
         }
     }
 }
