9be22163f98c4fdae8123c506d30616f67958f59
Piyawat Wongyat <mask30254@gmail.com>
2025-02-07 21:07:02 +0700

    ✨ feat[user.util.ts]: add validation rules for retrieving users by group name

--- src/utils/validators/user.util.ts
+++ src/utils/validators/user.util.ts
@@ -10,3 +10,12 @@ export const getUserValidationRules = () => [
     query("search").optional().isString().withMessage("Search must be a string"),
     query("logs").optional().isBoolean().withMessage("Logs must be a boolean"),
 ];
+
+
+export const getByGroupNameValidationRules = () => [
+    query("groupName").isString().withMessage("Group name must be a string"),
+    query("page").optional().isNumeric().withMessage("Page must be a number"),
+    query("pageSize").optional().isNumeric().withMessage("Page size must be a number"),
+    query("search").optional().isString().withMessage("Search must be a string"),
+    query("logs").optional().isBoolean().withMessage("Logs must be a boolean"),
+]
