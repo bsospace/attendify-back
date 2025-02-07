4b01a7cc261b99470f22d027188232187d580d1f
Piyawat Wongyat <mask30254@gmail.com>
2025-02-08 00:16:13 +0700

    ✨ feat[migration.sql, schema.prisma]: add avatar field to users model for enhanced user profile support

--- prisma/migrations/20250207170540_8_2_2025/migration.sql
+++ prisma/migrations/20250207170540_8_2_2025/migration.sql
@@ -150,6 +150,7 @@ CREATE TABLE "users" (
     "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "avatar" TEXT DEFAULT '',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
     "deleted_at" TIMESTAMP(3),
