a68f05b5ffbe2e48cbbd35d7bc6045fd094725c8
Piyawat Wongyat <mask30254@gmail.com>
2025-02-07 17:49:12 +0700

    ✨ feat(index.ts): add user route to API for user management

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { authRouter } from "./routes/auth.route";
 import { groupRouter } from "./routes/group.route";
 import { locationRouter } from "./routes/location.route";
 import { subLocationRouter } from "./routes/subLocation.route";
+import { userRouter } from "./routes/user.route";
 
 // Register routes
 app.use("/api/v1/auth", authRouter,);
 app.use("/api/v1/location", locationRouter);
 app.use("/api/v1/sublocation", subLocationRouter);
 app.use("/api/v1/group", groupRouter);
+app.use("/api/v1/user", userRouter);
