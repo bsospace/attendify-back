fd2a6592b221316575f7b339355845f27a105076
Piyawat Wongyat <mask30254@gmail.com>
2025-02-07 17:48:46 +0700

    ✨ feat[user.util.ts]: add validation rules for get user endpoint with optional parameters for pagination and search

---
+++ src/utils/validators/user.util.ts
@@ -0,0 +1,12 @@
+import { query } from "express-validator";
+
+
+
+
+// Validation rules for the get user endpoint
+export const getUserValidationRules = () => [
+    query("page").optional().isNumeric().withMessage("Page must be a number"),
+    query("pageSize").optional().isNumeric().withMessage("Page size must be a number"),
+    query("search").optional().isString().withMessage("Search must be a string"),
+    query("logs").optional().isBoolean().withMessage("Logs must be a boolean"),
+];
