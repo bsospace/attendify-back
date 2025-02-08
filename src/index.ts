830fe081f78409842fc8f079a715e69ff0eed74e
Piyawat Wongyat <mask30254@gmail.com>
2025-02-08 23:56:15 +0700

    ✨ feat[index.ts]: add project router for project management endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import app from "./app";
 import { authRouter } from "./routes/auth.route";
 import { groupRouter } from "./routes/group.route";
 import { locationRouter } from "./routes/location.route";
+import { projectRouter } from "./routes/project.route";
 import { subLocationRouter } from "./routes/subLocation.route";
 import { userRouter } from "./routes/user.route";
 
@@ -11,3 +12,4 @@ app.use("/api/v1/location", locationRouter);
 app.use("/api/v1/sublocation", subLocationRouter);
 app.use("/api/v1/group", groupRouter);
 app.use("/api/v1/user", userRouter);
+app.use('/api/v1/project', projectRouter);
